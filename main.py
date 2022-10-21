0673a78c40128756f60f5dd05b14835f5424c5e3
MiroslavZ <zenkov-miroslav@mail.ru>
2022-10-21 22:17:41 +0500

    Added very long comment

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 g = Github(os.getenv("TOKEN"))
 
-
+#HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA
 for c in g.get_user().get_repo("actionsTest").get_commits():
     print(c)
