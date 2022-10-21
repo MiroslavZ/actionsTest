d3d2223731537d19ab7cad74f7250a7328af0e0a
MiroslavZ <zenkov-miroslav@mail.ru>
2022-10-21 22:14:55 +0500

    Added some changes HAHAHAAH

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files,
-# tool windows, actions, and settings.
+from github import Github
+from dotenv import load_dotenv
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+load_dotenv()
+g = Github(os.getenv("TOKEN"))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for c in g.get_user().get_repo("actionsTest").get_commits():
+    print(c)
