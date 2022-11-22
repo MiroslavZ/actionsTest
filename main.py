effc6dd6a64f5c7928c6f7df263359dd73d12d37
MiroslavZ <zenkov-miroslav@mail.ru>
2022-11-22 20:40:51 +0500

    Experiment with repos content

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import os
 
-from github import Github
+from github import Github, ContentFile
 from dotenv import load_dotenv
+from github.Commit import Commit
+from github.PullRequest import PullRequest
+from github.Repository import Repository
 
 load_dotenv()
-g = Github(os.getenv("TOKEN"))
+token = os.getenv("TOKEN")
+g = Github(token)
 
-#HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA
-for c in g.get_user().get_repo("actionsTest").get_commits():
-    print(c)
+
+def get_and_update_content():
+    repo = g.get_user().get_repo("actionsTest")
+    sha = repo.get_contents('.env').sha
+    content_file = repo.update_file('.env','Updated .env file','testmessage',sha)
+    print(repo.get_contents('.env').decoded_content)
+
+
+if __name__ == '__main__':
+    get_content()
